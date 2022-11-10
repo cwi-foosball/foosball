892865b993f501382d7ae859c1f1c1341cb3a35c
Tobias Bora <tobias.bora@gmail.com>
2022-11-10 20:38:20 +0100

    Fix issue with missing overlay

--- configuration.nix
+++ configuration.nix
@@ -2,10 +2,10 @@
 # Would be cleaner to do that part with flake… but flake seems to be more ram consuming than legacy nix for now (some work are being done on that right now)
 let
   cwiFoosballWeb = builtins.fetchTarball {
-    url = https://github.com/cwi-foosball/foosball-web/archive/1008b34e3efe9abac80308f4ac21b22bc5fe2cf8.tar.gz;
+    url = https://github.com/cwi-foosball/foosball-web/archive/fef12114c9ef0cf67bc7e367ff3f7f4166455f78.tar.gz;
     # when you change the hash in url, write sha256 = ""; here, compile, and replace the hash with the hash
     # given in the error
-    sha256 = "sha256:0lnh2yjq7ba4q3fafiqhfyifkv3wqlph4g9ralndx2373xnmhrqg";
+    sha256 = "sha256:06bjp2472ylhnl4g77dr2mzknq9vp9srv2iym2f0fhxgkxg7ygzb";
   };
 in
 {  
@@ -17,6 +17,12 @@ in
   # Enable the "kiosk" (chromium stuff)
   services.CWIFoosballKiosk = {
     enableEverything = true;
-    kiosk.urlServer = "https://foosball.cwi.nl";
+    kiosk.urlServer = "localhost"; # The website is running locally
+  };
+  # Enable a local web server using the web server of foosball.cwi.nl for api/database
+  # (can't get access to foosball.cwi.nl)
+  services.CWIFoosballWeb = {
+    enable = true;
+    domainAPI = "https://foosball.cwi.nl";
   };
 }
